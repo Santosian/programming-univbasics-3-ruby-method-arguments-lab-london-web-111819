2625f3e76aab8b286aa9e58495dec27ef7ca9592
Santiago Molinari <santomolinari@gmail.com>
2019-11-04 22:21:11 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,11 +11,28 @@
 
 #greeting_and_presentation("Santiago")
 
+
+
+
+
+
+#def introduction_with_language(name, language = 'Ruby')
+#  puts "Hi, my name is #{name} and i am learning to program in #{language}"
+#end
+
+#introduction_with_language("Santiago")
+
+
+
+
+
+
+
 def introduction_with_language(name, language = 'Ruby')
   puts "Hi, my name is #{name} and i am learning to program in #{language}"
 end
 
-introduction_with_language("Santiago")
+introduction_with_language("Santiago", "Javascript")
 
 
 
